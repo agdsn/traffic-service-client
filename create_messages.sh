08cdbcf86ab9e5c40183f162625a173f02c16a70
Jan Losinski <losinski@wh2.tu-dresden.de>
2015-06-15 00:56:18 +0200

    Make messages a python package
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- create_messages.sh
+++ create_messages.sh
@@ -5,6 +5,7 @@ SRC_DIR=$HERE/traffic-service-protobuf
 DST_DIR=$HERE/src/traffic/messages
 
 mkdir -p $DST_DIR
+touch $DST_DIR/__init__.py
 
 for proto in ${SRC_DIR}/*.proto ; do
     protoc -I=$SRC_DIR --python_out=$DST_DIR ${proto}
